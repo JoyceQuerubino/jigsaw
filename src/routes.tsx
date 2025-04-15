8fdb408fa48b28857689c376163148a3087ce9b1
JoyceQuerubino <joycequerubino5@gmail.com>
2025-04-15 12:26:36 -0300

    fiz header

--- src/routes.tsx
+++ src/routes.tsx
@@ -57,8 +57,8 @@ export const router = createBrowserRouter([
                 path: '/game', 
                 element: <Game/>,
                 handle: {
-                    title: "Game",
-                    isMenu: false,
+                    // title: "Tema:",
+                    isMenu: true,
                     isGame: true
                 }
                 
