e1e55f8b6a0e5d4a759a5956a6a49bdb22d24584
JoyceQuerubino <joycequerubino5@gmail.com>
2025-08-12 01:10:58 -0300

    add resposivo home buttons sizes

--- src/pages/credits/credits.tsx
+++ src/pages/credits/credits.tsx
@@ -5,40 +5,15 @@ export function Credits() {
   return (
     <div className="credits-container">
       <div className="credits-content">
-        <div className="credits-background">
-          <div className="credits-content-wrapper">
-            <div className="credits-content-column">
-              <div className="credits-content-items">
-                <h3>Orientador</h3>
-                <p>Dr. Carlos Marcelo Gurjão de Godoy</p>
-              </div>
+        <div className="credits-header-white"></div>
+          <div className="credits-header">
+            <h1>CRÉDITOS</h1>
+          </div>
 
-              <div className="credits-content-items">
-                <h3>Coorientadoras</h3>
-                <p>Dra. Regina Célia Coelho</p>
-                <p>Dra. Vanessa Andrade Pereira</p>
-              </div>
-
-              <div className="credits-content-items">
-                <img src={unifespLogo} alt="Logo UNIFESP" className="unifesp-logo" />
-              </div>
-            </div>
-
-            <div className="credits-content-column">
-              <div className="credits-content-items">
-                <h3>Mestranda</h3>
-                <p>Geisse Rosane de Faria Dias Costa</p>
-              </div>
-
-              <div className="credits-content-items">
-                <h3>Desenvolvimento</h3>
-                <p>Joyce Querubino de Oliveira</p>
-              </div>
-
-              <div className="credits-content-items">
-                <h3>Personagem</h3>
-                <p>Otavio Pozzebon Scalari</p>
-              </div>
+        <div className="credits-white">
+          <div className="credits-brow">
+            <div className="credits-yellow">
+              <div className="credits-orange">{/* <h1>FORCA</h1> */}</div>
             </div>
           </div>
         </div>
