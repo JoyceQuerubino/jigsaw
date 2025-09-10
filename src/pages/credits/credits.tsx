b0bcd49ed5a33b3d3c810a251d8854e0439ac5ae
JoyceQuerubino <joycequerubino5@gmail.com>
2025-09-10 00:48:36 -0300

    add tutorials styles and change credits card component

--- src/pages/credits/credits.tsx
+++ src/pages/credits/credits.tsx
@@ -1,51 +1,39 @@
 import "./credits.css";
 import unifespLogo from "../../assets/images/unifesp.png";
+import { CreditsCard } from "../../components/CreditsCard";
 
 export function Credits() {
   return (
     <div className="credits-container">
-      <div className="credits-content">
-        <div className="credits-header-white"></div>
-        <div className="credits-header">
-          <h1>CRÉDITOS</h1>
-        </div>
+      <CreditsCard>
+          <div className="credits-col">
+            <div className="credits-col-row">
+              <h3>Orientador</h3>
+              <p>Dr. Carlos Marcelo Gurjão de Godoy</p>
+            </div>
+            <div className="credits-col-row">
+              <h3>Coorientadoras</h3>
+              <p>Dra. Regina Célia Coelho</p>
+              <p>Dra. Vanessa Andrade Pereira</p>
+            </div>
 
-        <div className="credits-white">
-          <div className="credits-brow">
-            <div className="credits-yellow">
-              <div className="credits-orange">
-                <div className="credits-col">
-                  <div className="credits-col-row">
-                    <h3>Orientador</h3>
-                    <p>Dr. Carlos Marcelo Gurjão de Godoy</p>
-                  </div>
-                  <div className="credits-col-row">
-                    <h3>Coorientadoras</h3>
-                    <p>Dra. Regina Célia Coelho</p>
-                    <p>Dra. Vanessa Andrade Pereira</p>
-                  </div>
-
-                  <img src={unifespLogo} alt="UNIFESP Logo" className="unifesp-logo" />
-                </div>
-                <div className="credits-col">
-                  <div className="credits-col-row">
-                    <h3>Mestranda</h3>
-                    <p>Geisse Rosane de Faria Dias Costa</p>
-                  </div>
-                  <div className="credits-col-row">
-                    <h3>Desenvolvimento</h3>
-                    <p>Joyce Querubino de Oliveira</p>
-                  </div>
-                  <div className="credits-col-row">
-                    <h3>Personagem</h3>
-                    <p>Otavio Pozzebon Scalari</p>
-                  </div>
-                </div>
-              </div>
+            <img src={unifespLogo} alt="UNIFESP Logo" className="unifesp-logo" />
+          </div>
+          <div className="credits-col">
+            <div className="credits-col-row">
+              <h3>Mestranda</h3>
+              <p>Geisse Rosane de Faria Dias Costa</p>
+            </div>
+            <div className="credits-col-row">
+              <h3>Desenvolvimento</h3>
+              <p>Joyce Querubino de Oliveira</p>
+            </div>
+            <div className="credits-col-row">
+              <h3>Personagem</h3>
+              <p>Otavio Pozzebon Scalari</p>
             </div>
           </div>
-        </div>
-      </div>
+        </CreditsCard>
     </div>
   );
 }
