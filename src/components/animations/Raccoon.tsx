9713786a0be3744af5ef256736c26b0d04f7fc50
JoyceQuerubino <joycequerubino5@gmail.com>
2025-09-10 01:32:42 -0300

    add others

--- src/components/animations/Raccoon.tsx
+++ src/components/animations/Raccoon.tsx
@@ -1,10 +1,14 @@
 import { Alignment, Fit, Layout, useRive, useStateMachineInput } from "@rive-app/react-canvas";
 import { useEffect, useState } from "react";
 
-export const Raccoon = () => {
+export interface RacconAnimationProps {
+  initialState: number, 
+  animationNumber?: number;
+}
+
+export const Raccoon = ({initialState = 0, animationNumber = 2}: RacconAnimationProps) => {
   // Name of the State Machine for this animation
   const STATE_MACHINE_NAME = "State Machine 1";
-  // Name of the input used in the state machine to trigger states
   const STATE_MACHINE_INPUT_NAME = "Number 1";
   const [flameRateValue, setFlameRateValue] = useState(0);
 
@@ -12,10 +16,10 @@ export const Raccoon = () => {
     src: "/src/assets/guaxinim.riv",
     autoplay: true,
     stateMachines: STATE_MACHINE_NAME,
-    layout: new Layout({ fit: Fit.None, alignment: Alignment.Center }),
-  
+    layout: new Layout({ fit: Fit.Cover, alignment: Alignment.Center }),
     useOffscreenRenderer: true, 
     onStateChange: (e) => {
+      console.log('State change event:', e.data);
       if(e.data[0] === 'ide'){
         setFlameRateValue(0)
       }
@@ -30,12 +34,21 @@ export const Raccoon = () => {
 
   useEffect(() => {
     if (flameRateInput) {
+      console.log('Setting flameRateInput.value to:', flameRateValue);
       flameRateInput.value = flameRateValue;
     }
   }, [flameRateValue, flameRateInput]);
 
+  // Garantir que o estado inicial seja aplicado quando o input estiver disponível
+  // useEffect(() => {
+  //   if (flameRateInput && flameRateValue !== initialState) {
+  //     console.log('Forcing initial state after input is ready:', initialState);
+  //     setFlameRateValue(initialState);
+  //   }
+  // }, [flameRateInput, initialState]);
+
   return (
-    <div style={{ width: 712, height: 280}}>
+    <div style={{ width: 842, height: 410}}>
       <RiveComponent 
         style={{ 
           backgroundColor: "transparent",
